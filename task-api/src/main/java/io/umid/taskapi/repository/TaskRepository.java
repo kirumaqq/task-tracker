5829644c498047e812b4ae54d2cd4d4c2b944fa5
umid <umid1504@gmail.com>
2024-04-05 12:18:51 +0500

    fix: specify JPQL to solve n+1 problem

--- task-api/src/main/java/io/umid/taskapi/repository/TaskRepository.java
+++ task-api/src/main/java/io/umid/taskapi/repository/TaskRepository.java
@@ -3,11 +3,24 @@ package io.umid.taskapi.repository;
 import io.umid.taskapi.entity.Task;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 public interface TaskRepository extends CrudRepository<Task, Long>, JpaSpecificationExecutor<Task> {
 
 
     Page<Task> getAllByUserId(String userId, Pageable pageable);
+
+    @Query("""
+            select t, u
+            from Task t
+            join fetch User u
+            on t.user = u
+            """)
+    @Override
+    List<Task> findAll(Specification<Task> spec);
 }
